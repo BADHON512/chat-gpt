a26bc196b026e7bafbb3b61b44715db22201a368
BADHON512 <mylaptop512@gmail.com>
2025-04-01 20:48:19 +0600

    20

--- app/page.tsx
+++ app/page.tsx
@@ -57,6 +57,7 @@ const ChatBot = () => {
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"
         >
           Send
+          
         </button>
       </div>
     </div>
